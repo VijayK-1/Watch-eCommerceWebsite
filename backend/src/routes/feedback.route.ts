9402a9010a154b8f6d1c192fcf91028b5c0bcf24
vijay k <vijaymsk123@gmail.com>
2025-10-26 12:07:42 +0530

    fix git lock issue

--- backend/src/routes/feedback.route.ts
+++
@@ -1,8 +0,0 @@
-import  express from "express";
-import { feedbackmsg } from "../controllers/feedback.controller";
-import authMiddleware from "../middleware/auth";
-
-const router=express.Router()
-router.post('/msg',authMiddleware,feedbackmsg)
-
-export default router
