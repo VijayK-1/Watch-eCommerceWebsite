4bd1e81d1d3e8f203767a0813374e813a1830deb
vijay k <vijaymsk123@gmail.com>
2025-10-30 05:00:19 +0530

    Update MenProducts.jsx

--- frontend/src/components/MenProducts.jsx
+++ frontend/src/components/MenProducts.jsx
@@ -66,7 +66,7 @@ import 'aos/dist/aos.css';
 function MenProducts() {
   const handleAdd = async (item) => {
     const token = localStorage.getItem('token');
-    await axios.post('/api/cart/add', {
+    await axios.post('https://watch-ecommercewebsite-backend.onrender.com/api/cart/add', {
       productId: item.text,
       name: item.text,
       price: item.price,
@@ -87,7 +87,7 @@ function MenProducts() {
   const addToFavotite=async(item)=>{
         const token = localStorage.getItem('token');
        try {
-    await axios.post('/api/fav/addFav',{
+    await axios.post('https://watch-ecommercewebsite-backend.onrender.com/api/fav/addFav',{
       productId:item.text,
       image:item.image,
       name:item.text,
